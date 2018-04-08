fb00067dd916a7320ca603c15b1afbbe6fe9ff9a
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2018-04-08 15:55:02 +0300

    Install padmiss to PRO cabinet

---
+++ roles/padmiss/templates/config.py
@@ -0,0 +1,13 @@
+from hid import RFIDReader
+from util import FIFOReader
+
+url = 'http://api.padmiss.com:3020'
+apikey = '{{ padmiss_api_key }}'
+scores_dir = '/home/stepmania/.stepmania-5.0/Save/Padmiss'
+backup_dir = '/opt/padmiss-daemon-backups'
+profile_dir = 'StepMania 5'
+
+readers = {
+    '/tmp/padmiss-daemon-p1' : lambda: RFIDReader(idVendor=0x08ff, idProduct=0x0009, bus={{ rfid_reader_p1_bus }}, port_number={{ rfid_reader_p1_port_number }}),
+    '/tmp/padmiss-daemon-p2' : lambda: RFIDReader(idVendor=0x08ff, idProduct=0x0009, bus={{ rfid_reader_p2_bus }}, port_number={{ rfid_reader_p2_port_number }}),
+}
--- roles/stepmania/files/start.sh
+++ roles/stepmania/files/start.sh
@@ -14,11 +14,15 @@ xset s off
 # hide mouse cursor if not moved
 unclutter -idle 1 -root &
 
-# try to clone displays for streming – but if it's not possible for some reason , that's fine as well
+# try to clone displays for streming – but if it's not possible for some reason, that's fine as well
 xrandr --output HDMI-0 --same-as DVI-D-0 --mode 1280x720 --scale-from 1920x1080 || true
 
 # force vsync off
 export __GL_SYNC_TO_VBLANK=0
+
+# clear padmiss profile directories
+rm -rf /tmp/padmiss-daemon-p1
+rm -rf /tmp/padmiss-daemon-p2
 
 pasuspender /stepmania/stepmania
 

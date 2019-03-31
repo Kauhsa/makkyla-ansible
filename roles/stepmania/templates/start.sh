9f0b55ed0205d42716ee26ca89a7d4158c149393
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2019-03-31 16:59:34 +0300

    Attempt to fix PIUIO keybind issues

--- roles/stepmania/templates/start.sh
+++ roles/stepmania/templates/start.sh
@@ -28,6 +28,29 @@ export __GL_SYNC_TO_VBLANK=0
 rm -rf /tmp/padmiss-daemon-p1
 rm -rf /tmp/padmiss-daemon-p2
 
+{% if piuio_bind_fix %}
+# HAX: make sure PIUIO is bound on correct keymap index
+piuio_index=0
+IFS="
+"
+# ls -f is important - stepmania iterates devices on same order
+for device in $(ls -f /sys/class/input | grep input); do
+  device_name=$(cat "/sys/class/input/$device/name")
+
+  # is it the lovely piuio? could it be?
+  if [[ $device_name == *"PIUIO"* ]]; then
+    echo "PIUIO found in index ${piuio_index}"
+    sed "s/JoyXX/Joy1${piuio_index}/g" /home/stepmania/Keymaps.template.ini > /home/stepmania/.stepmania-5.0/Save/Keymaps.ini
+    break
+  fi
+
+  # fine, it is another joystick? means that PIUIO is bumped one index further
+  if ls /sys/class/input/$device/js*; then
+    piuio_index=$((piuio_index + 1))
+  fi
+done
+{% endif %}
+
 pasuspender /stepmania/stepmania
 
 if [[ $? -ne 0 ]]; then
